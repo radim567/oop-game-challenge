a393d74ec371832e095b3ce6efec4c9efa19daac
Radim Vrška <87022843+radim567@users.noreply.github.com>
2023-05-18 19:32:13 +0200

    Players set

--- script.js
+++ script.js
@@ -12,4 +12,13 @@ Player.prototype.gainXp = function (xp) {
   }
 };
 
+Player.prototype.describe = function () {
+  return `${this.name} is at level ${this.lvl} with ${this.points} XP`;
+};
+
 console.log(Player.prototype);
+
+const player1 = new Player("Jonas");
+const player2 = new Player("Diana");
+console.log(player1);
+console.log(player2);
